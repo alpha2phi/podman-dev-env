ca5e70ac277020b6a248675d570de73557f0f2e8
mengwangk <mengwangk@gmail.com>
2024-07-25 10:42:46 +0800

    initial commit

--- local-dev/java/scripts/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WORK_DIR=/app
-
-JAVA_OPTS="${JAVA_OPTS} \
-  -Djava.security.egd=file:/dev/urandom \
-  -javaagent:${WORK_DIR}/dd-java-agent.jar"
-
-exec java ${JAVA_OPTS} -jar ${WORK_DIR}/app.jar $JAVA_ARGS
--- local-dev/java/src/main/java/com/cargill/demo/repository/GreetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cargill.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cargill.demo.model.Greeting;
-
-public interface GreetingRepository extends JpaRepository<Greeting, Integer> {
-    Greeting findByName(String eid);
-}
